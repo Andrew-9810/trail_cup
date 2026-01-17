ef2970c04b849c5cae4960f2f5103775848c6fc8
Golovanov Andrey <andrew-9810@yandex.ru>
2026-01-17 22:47:47 +0300

    Добавлял поля в модель, правил модели

--- trail_cup/person/models.py
+++ trail_cup/person/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+class Season(models.Model):
+    """Сезон забегов."""
+    title = models.IntegerField()
+
+
 class Person(models.Model):
     """Участники."""
     MAN = 'M'
@@ -12,23 +17,24 @@ class Person(models.Model):
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default=MAN)
     birthday = models.DateField()
     keys = models.CharField(
-        max_length=60, default=f'{surname}{name}{birthday}', blank=True
+        max_length=64, default=f'{surname}{name}{birthday}', blank=True
     )
+
+class TitleGroup(models.Model):
+    """Заголовки групп."""
+    title = models.CharField(max_length=20)
 
 
 class Group(models.Model):
     """Группы участников."""
-    title = models.CharField(max_length=20)
+    season = models.ForeignKey(Season, on_delete=models.PROTECT, default=1900)
+    title = models.ForeignKey(TitleGroup, on_delete=models.PROTECT)
     year_max = models.DateField()
     year_min = models.DateField()
 
-class Season(models.Model):
-    """Сезон забегов."""
-    title = models.IntegerField()
-
 class Scores(models.Model):
     """Очки за результат."""
-    season = models.ForeignKey(Season, on_delete=models.PROTECT)
+    season = models.ForeignKey(Season, on_delete=models.PROTECT, default=1900)
     result_place = models.IntegerField()
     scores = models.IntegerField()
 
@@ -37,10 +43,11 @@ class Run(models.Model):
     """Трейл забеги."""
     DAY = 'D'
     NIGHT = 'N'
-    TYPE_CHOICES = [(DAY, 'M'), (NIGHT, 'Ж')]
+    TYPE_CHOICES = [(DAY, 'День'), (NIGHT, 'Ночь')]
+    season = models.ForeignKey(Season, on_delete=models.PROTECT, default=1900)
     title = models.CharField(max_length=40)
     type_run = models.CharField(
-        max_length=1, choices=TYPE_CHOICES, default=DAY
+        max_length=4, choices=TYPE_CHOICES, default=DAY
     )
     data_run = models.DateField()
 
@@ -50,4 +57,5 @@ class Result(models.Model):
     person = models.ForeignKey(Person, on_delete=models.PROTECT)
     result_place = models.IntegerField()
     result_time = models.DateTimeField()
-    # group =
+    distance = models.IntegerField(default=0)
+    group = models.ForeignKey(Group, on_delete=models.PROTECT, default=1)

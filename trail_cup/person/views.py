0379d1c5ab7cc073962169720b1eca9386e38ea1
Golovanov Andrey <andrew-9810@yandex.ru>
2026-01-23 12:14:36 +0300

    Пробовал загрузить результатыÐ

--- trail_cup/person/views.py
+++ trail_cup/person/views.py
@@ -1,5 +1,8 @@
 import csv
 
+from .models import Result, Person, Run, Group
+
+
 from django.shortcuts import render
 
 NAME = 0
@@ -7,6 +10,7 @@ YEAR = 1
 RES_TIME = 2
 RES_PLACE = 3
 DISTANCE = 4
+GENDER = 5
 
 def read_csv(path):
     """Получает путь к файлу csv отдает считанный в переменную файл."""
@@ -16,7 +20,7 @@ def read_csv(path):
         for row in reader:
             data = [
                 row[NAME], row[YEAR], row[RES_TIME],
-                row[RES_PLACE], row[DISTANCE]
+                row[RES_PLACE], row[DISTANCE], row[GENDER]
             ]
             persons.append(data)
     return persons
@@ -24,11 +28,27 @@ def read_csv(path):
 def parce_csv(file):
     """Берет строчку файла и записывает ее в БД."""
     for i in file:
-        print(i)
+        fi = i[NAME].split()
+        person = Person.objects.create(
+            surname = fi[0],
+            name = fi[1],
+            gender = i[GENDER],
+            birthday = f'{i[YEAR]}-01-01'
+        )
+        run = Run.objects.get(id=1)
+        group = Group.objects.get(id=1)
+        Result.objects.create(
+            run = run,
+            person = person,
+            result_place = i[RES_PLACE],
+            result_time = i[RES_TIME], #['“1:37:09” value has an invalid format. It must be in YYYY-MM-DD HH:MM[:ss[.uuuuuu]][TZ] format.']
+            distance = i[DISTANCE],
+            group = group
 
-def main_pr():
-    path_csv = '2025.csv'
+        )
+
+def main_pr(request):
+    path_csv = 'D:\\DEV\\trail_cup\\trail_cup\\person\\2025.csv'
     file_csv = read_csv(path_csv)
     parce_csv(file_csv)
-
-main_pr()
+    return None
